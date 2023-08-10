8bb79206c692137d4e11a1f61360bc31e7f38854
iamthejafar <jafarjalali128@gmail.com>
2023-08-10 09:04:57 +0530

    Initial Commit

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,7 +1,7 @@
 class Constants{
-  static String TOP_HEADLINES_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=292d89acfdbd4a8fad28ee67c6d2fdee';
+  static String TOP_HEADLINES_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=';
 
   static String healinesFor(String kw){
-    return "https://newsapi.org/v2/everything?q="+"$kw"+"&from=2023-08-08&sortBy=popularity&apiKey=292d89acfdbd4a8fad28ee67c6d2fdee";
+    return "https://newsapi.org/v2/everything?q="+"$kw"+"&from=2023-08-08&sortBy=popularity&apiKey=";
   }
 }
